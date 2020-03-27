fd25aaaf6fe77d7cccb89e407f3de5da9737f718
Ahren Pradhan <ahrenpradhan@gmail.com>
2020-03-27 15:59:01 +0530

    v1.0.0

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// import 'normalize.css/normalize.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+// import './styles/styles.scss';
+
+ReactDOM.render( <Main /> , document.getElementById('app'));
